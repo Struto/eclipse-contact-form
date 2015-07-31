c36d1821f5283f199f6e7b08e9d33e9ae4d83a54
Archie Makuwa <archie@aatsol.co.za>
2015-07-31 14:43:09 +0200

    First commmit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# eclipse-contact-form
-A temporary HubSpot form for Eclipse. 
+# Eclipse Temporary Contact Form Script
+A temporary HubSpot form for Eclipse powered by HubSpot. It runs from the Struto server. 
+Any questions, please ask: Archie Makuwa (archie@aatsol.co.za)
+
+
+## Git Controlled
+Script has to be changed here and pulled on the server.
---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <title>Eclipse Contact Form </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <div class="contact-form">
+
+        <!-- Add your site or application content here -->
+        <h2 class="form">Enquiry Form</h2>
+        
+        <!--[if lte IE 8]>
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+        <![endif]-->
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+        <script>
+          hbspt.forms.create({ 
+            css: '',
+            portalId: '181151',
+            formId: '8e17bab0-1655-402d-b055-07826334a79d'
+          });
+        </script>
+
+    </div>
+
+
+
+    <!-- Stylesheet -->
+    <style>
+
+        body{
+            color: #6e6e6e;
+            background: rgba(0, 137, 208, 0.1);
+            font-family: Helvetica,Arial,FreeSans,sans-serif;
+            font-size: 14px;
+        }
+
+        .contact-form {  border: 1px solid blue;
+            display: block;
+            width: 300px;
+        }
+
+        .contact-form h2.form {
+            color: #303030 !important;
+            font-size: 25px !important;
+            font-weight: normal !important;
+            line-height: 30px !important;
+            font-family: Helvetica,Arial,FreeSans,sans-serif;
+            padding-left: 10px;
+        }
+
+        .contact-form form { 
+            padding-left: 30px;
+            padding-right: 30px;
+            padding-bottom: 30px;
+        }
+
+        .contact-form  form div div.hs-form-field {
+            margin: 10px 0;
+        }
+
+        .contact-form form .hs-form-field label span.hs-form-required {
+            color: red;
+            margin-left: 5px;
+        }
+
+        .contact-form form .hs-form-field ul.hs-error-msgs.inputs-list {
+            padding: 0;
+            margin: 0;
+        }
+
+        .contact-form form .hs-form-field ul.hs-error-msgs.inputs-list li {
+            color: red;
+            font-size: 10px;
+            list-style-type: none;
+        }
+
+        textarea, 
+        input, 
+        input[type="text"], 
+        input[type="password"], 
+        input[type="datetime"], 
+        input[type="datetime-local"], 
+        input[type="date"], 
+        input[type="month"], 
+        input[type="time"], 
+        input[type="week"], 
+        input[type="number"], 
+        input[type="email"], 
+        input[type="url"], 
+        input[type="search"], 
+        input[type="tel"], 
+        input[type="color"] { 
+            background-color: #ffffff;
+            border: 1px solid #d5d5d5;
+            border-radius: 4px;
+            color: #6e6e6e;
+            font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+            font-size: 14px;
+            margin: 0;
+            padding: 4px 6px;
+            vertical-align: middle; 
+            width: 206px;
+        }
+
+        .contact-form form input.hs-button {
+
+            width: 90px;
+        }
+
+
+    </style>
+
+
+</body>
+
+</html>
+
